df7802ccdfe41039f440642af4adfce597780f7a
TmothyMartel <t.m.martel@gmail.com>
2018-09-07 09:43:01 -0400

    setting up cors

--- server.js
+++ server.js
@@ -20,16 +20,15 @@ const app = express();
 // Logging
 app.use(morgan("common"));
 app.use(express.static("public"));
-//enabling CORS
-app.use(
-  cors({
-    origin: DATABASE_URL
-  })
-);
+// //enabling CORS
+// app.use(cors());
 
 // CORS
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://scenic-client.herokuapp.com/"
+  );
   res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
   if (req.method === "OPTIONS") {
